fe5458735e4285c35a0b6a69fa114cb0fccb7b2c
Calvin Chern <calvin.chern24@gmail.com>
2023-01-21 21:55:58 -0500

    schema and connection javascript update

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+const consoletable = require('console.table');
 
 // Connect to database
 const db = mysql.createConnection({
--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,24 @@ DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS departments;
 
-CREATE TABLE departments 
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
 
-CREATE TABLE roles
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    ON DELETE SET NULL
+);
 
-CREATE TABLE employees
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER REFERENCES employees,
+    ON DELETE SET NULL
+);
